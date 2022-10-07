56dce15fa74bdc966f954de5b5fd7f5c095f4f49
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-10-07 16:10:50 +0700

    sss

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 8, 5, 6, 7, 9, 1};
-        int index_del = 0;
+        int[] arr = {3, 3, 5, 3, 3, 9, 3};
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the element to be deleted: ");
         int number = scanner.nextInt();
         for (int i = 0; i < arr.length; i++){
             if (number == arr[i]){
-                index_del = i;
+                Delete(arr,i);
+                i--;
+                arr[arr.length - 1] = 0;
             }
         }
-        for (int i = index_del; i < arr.length - 1; i++){
-            arr[i] = arr[i+1];
-        }
-        arr[arr.length - 1] = 0;
+
         for (int i = 0;i < arr.length;i++){
             System.out.print(arr[i] + "\t");
+        }
+    }
+    public static void Delete (int[] array,int x){
+        for (int j = x; j < array.length - 1 ; j++){
+            array[j] = array[j+1];
         }
     }
 }
